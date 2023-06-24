b43e06646ce002ae59dea6c089f855ff1e5c145f
pontakornth <pontakorn61@gmail.com>
2023-06-24 18:27:46 +0700

    Add fake button

--- plugin/after.js
+++ plugin/after.js
@@ -1,6 +1,89 @@
 // this code will be executed after page load
 var intervalLoop
 var timeout
+
+const svg = `
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   version="1.1"
+   id="svg2"
+   height="25"
+   width="25"
+   sodipodi:docname="close_icon_grey.svg"
+   inkscape:export-filename="D:\Загрузки\close_icon_black2.svg.png"
+   inkscape:export-xdpi="72.959999"
+   inkscape:export-ydpi="72.959999"
+   inkscape:version="0.92.3 (2405546, 2018-03-11)">
+  <sodipodi:namedview
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1"
+     objecttolerance="10"
+     gridtolerance="10"
+     guidetolerance="10"
+     inkscape:pageopacity="0"
+     inkscape:pageshadow="2"
+     inkscape:window-width="1366"
+     inkscape:window-height="706"
+     id="namedview3731"
+     showgrid="false"
+     inkscape:zoom="15.245222"
+     inkscape:cx="5.6822049"
+     inkscape:cy="12.495936"
+     inkscape:window-x="-8"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="imagebot_2" />
+  <defs
+     id="defs13" />
+  <metadata
+     id="imagebot_6">image/svg+xml<rdf:RDF>
+  <cc:Work
+     rdf:about="">
+    <dc:format>image/svg+xml</dc:format>
+    <dc:type
+       rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+    <dc:title></dc:title>
+  </cc:Work>
+</rdf:RDF>
+</metadata>
+  <g
+     transform="translate(3.9661017,3.5677966)"
+     id="imagebot_2"
+     label="Layer 1">
+    <path
+       inkscape:connector-curvature="0"
+       stroke-miterlimit="4"
+       d="M -2.5783352e-4,-0.00146808 17.435473,18.212367"
+       id="imagebot_5"
+       style="opacity:1;fill:#5f6368;stroke:#5f6368;stroke-width:3.23161912;stroke-linecap:round;stroke-miterlimit:4;fill-opacity:1;stroke-opacity:1" />
+    <path
+       inkscape:connector-curvature="0"
+       stroke-miterlimit="4"
+       d="M -2.5783352e-4,18.212367 17.435473,-0.00146808"
+       id="imagebot_4"
+       style="opacity:1;fill:#5f6368;stroke:#5f6368;stroke-width:3.23161912;stroke-linecap:round;stroke-miterlimit:4;fill-opacity:1;stroke-opacity:1" />
+    <title
+       id="title9">Layer 1</title>
+  </g>
+</svg>
+`
+
+const attachFakeButton = (node) => {
+	const temp = document.createElement('div')
+	temp.innerHTML = svg
+	temp.style.position = 'absolute'
+	temp.style.top = '0'
+	temp.style.right = '0'
+	temp.style.padding = '1rem'
+	node.appendChild(temp)
+}
 const loopElements = (query) => {
 	const elements = document.querySelectorAll(query)
 	elements.forEach((element) => {
@@ -10,7 +93,6 @@ const loopElements = (query) => {
 		)
 	})
 }
-
 const startInterval = () => {
 	intervalLoop = setInterval(() => {
 		loopElements('ytd-rich-item-renderer.style-scope')
@@ -52,6 +134,7 @@ const appendImage = (node, src) => {
 		div.style.backgroundImage = 'url(' + src + ')'
 		applyAdsStyle(div)
 
+		attachFakeButton(div)
 		div.addEventListener('click', () => {
 			div.remove()
 			clearInterval(intervalLoop)
@@ -81,6 +164,7 @@ const addBanners = () => {
 		top.className = 'top-youtube-basic'
 		top.style.zIndex = '9999'
 		applyBackground(top)
+		attachFakeButton(top)
 		top.addEventListener('click', () => {
 			top.remove()
 			clearInterval(intervalLoop)
@@ -106,6 +190,7 @@ const addBanners = () => {
 		bottom.className = 'bottom-youtube-basic'
 		bottom.style.zIndex = '1000'
 		applyBackground(bottom)
+		attachFakeButton(bottom)
 		bottom.addEventListener('click', () => {
 			bottom.remove()
 			clearInterval(intervalLoop)
@@ -131,6 +216,7 @@ const addBanners = () => {
 		left.className = 'left-youtube-basic'
 		left.style.zIndex = '9999'
 		applyBackground(left)
+		attachFakeButton(left)
 		left.addEventListener('click', () => {
 			left.remove()
 			clearInterval(intervalLoop)
@@ -155,6 +241,7 @@ const addBanners = () => {
 		right.className = 'right-youtube-basic'
 		right.style.zIndex = '9999'
 		applyBackground(right)
+		attachFakeButton(right)
 		right.addEventListener('click', () => {
 			right.remove()
 			clearInterval(intervalLoop)
