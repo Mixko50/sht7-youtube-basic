952c9fe2125de67afe65a67f91890deb103772ab
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2023-06-24 11:52:52 +0700

    add: prettierrc

--- before.js
+++ before.js
@@ -1,4 +1,4 @@
 // this code will be executed before page load
-(function() {
-  console.log('before.js executed');
-})();
+;(function () {
+	console.log('before.js executed')
+})()
--- execute.js
+++ execute.js
@@ -1,4 +1,4 @@
 // this code will be executed when the extension's button is clicked
-(function() {
-  console.log('execute.js executed');
-})();
+;(function () {
+	console.log('execute.js executed')
+})()
