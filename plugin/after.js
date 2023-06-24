cc29a3591dda4328dffd4c081ebca16188efe42d
Floaty_KT <69688279+FloatKasemtan@users.noreply.github.com>
2023-06-24 16:42:49 +0700

    feat: auto show ads script

--- plugin/after.js
+++ plugin/after.js
@@ -1,4 +1,54 @@
 // this code will be executed after page load
-(function() {
-  console.log('after.js executed');
-})();
+
+const loopElements = (query) => {
+	const elements = document.querySelectorAll(query)
+	elements.forEach((element) => {
+		appendImage(
+			element,
+			'https://media.tenor.com/eJYeR7tKDs8AAAAC/ufabet-%E0%B8%9E%E0%B8%99%E0%B8%B1%E0%B8%99%E0%B8%AD%E0%B8%AD%E0%B8%99%E0%B9%84%E0%B8%A5%E0%B8%99%E0%B9%8C.gif'
+		)
+	})
+}
+
+const applyAdsStyle = (node) => {
+	node.style.position = 'absolute'
+	node.style.width = '100%'
+	node.style.height = '100%'
+	node.style.top = '0'
+	node.style.left = '0'
+	node.style.zIndex = '1000'
+	node.style.backgroundRepeat = 'no-repeat'
+	node.style.backgroundSize = 'cover'
+}
+
+const appendImage = (node, src) => {
+	node.style.position = 'relative'
+
+	if (!node.querySelector('.youtube-basic')) {
+		const div = document.createElement('a')
+
+		div.href = 'http://facebook.com'
+		div.target = '_blank'
+		div.className = 'youtube-basic'
+		div.style.backgroundImage = 'url(' + src + ')'
+		applyAdsStyle(div)
+
+		div.addEventListener('click', () => {
+			div.remove()
+		})
+
+		node.appendChild(div)
+	}
+}
+
+const interval = (debounce, func) => {
+	setInterval(() => {
+		func()
+	}, debounce)
+}
+
+;(function () {
+	interval(5000, () => loopElements('ytd-rich-item-renderer.style-scope'))
+	interval(5000, () => loopElements('ytd-compact-video-renderer'))
+	interval(5000, () => loopElements('ytd-comment-renderer'))
+})()
