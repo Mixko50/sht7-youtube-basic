b881767a1f96b6b4aeea169b9378e34ec356b035
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2023-06-24 16:33:08 +0700

    fix: ads model

--- server/database/ads.go
+++ server/database/ads.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Ads struct {
-	Id        *uint64    `json:"id" gorm:"primary_key"`
-	Name      *string    `json:"name" gorm:"type:varchar(255);not null"`
-	ImageUrl  *string    `json:"image_url" gorm:"type:text;not null"`
-	TagetLink *string    `json:"taget_link" gorm:"type:text;not null"`
-	AdsType   *AdsType   `json:"ads_type" gorm:"type:enum('skyscraper', 'leaderboard', 'square', 'large_rectangle');not null"`
-	Count     *uint64    `json:"count" gorm:"not null"`
-	CreatedAt *time.Time `json:"created_at" gorm:"not null"`
-	UpdatedAt *time.Time `json:"updated_at" gorm:"not null"`
+	Id         *uint64    `json:"id" gorm:"primary_key"`
+	Name       *string    `json:"name" gorm:"type:varchar(255);not null"`
+	ImageUrl   *string    `json:"image_url" gorm:"type:text;not null"`
+	TargetLink *string    `json:"target_link" gorm:"type:text;not null"`
+	AdsType    *AdsType   `json:"ads_type" gorm:"type:enum('skyscraper', 'leaderboard', 'square', 'large_rectangle');not null"`
+	Count      *uint64    `json:"count" gorm:"not null"`
+	CreatedAt  *time.Time `json:"created_at" gorm:"not null"`
+	UpdatedAt  *time.Time `json:"updated_at" gorm:"not null"`
 }
 
 type AdsType string
